fe38d63ec779b1310a24a536f1fa9ad8d40f90d2
MrLeonard7 <dbecerra655@gmail.com>
2024-01-29 19:52:06 -0500

    Ordemanos todo en funciuones solucionamos inconvenientes ccon rellenar el formulario y terminamos la funcion de aceptar terminos y terminar la compra

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def create_url():
     url = "https://speedlogic.com.co/producto/tarjeta-de-video-asus-geforce-gt-1030-ddr5-oc-phoenix-1fan-2-gigas/"
     return url
 
+
 def connection_to_page(url):
     session = HTMLSession()
     page = session.get(url)
@@ -25,6 +26,46 @@ def review_stock(page):
         sleep(30)
 
 
+def created_driver_of_page(url_product):
+    driver = webdriver.Firefox()
+    driver.get(url_product)
+    return driver
+
+
+def accept_cookies(driver_page):
+    driver_page.find_element(by=By.XPATH, value='/html/body/div[3]/div/div/div/div[2]/button[2]').click()
+    sleep(2)
+
+
+def process_of_buy(driver_page):
+    driver_page.find_element(by=By.NAME, value="add-to-cart").click()
+    sleep(2)
+    final_buy = driver_page.find_element(by=By.CLASS_NAME, value="wc-proceed-to-checkout")
+    final_buy.find_element(by=By.TAG_NAME, value="a").click()
+    sleep(2)
+
+
+def complete_formulary(driver_page):
+    facturation_formulary = driver_page.find_element(by=By.CLASS_NAME, value="woocommerce-billing-fields")
+    facturation_formulary.find_element(by=By.NAME, value="billing_first_name").send_keys("David")
+    facturation_formulary.find_element(by=By.NAME, value="billing_last_name").send_keys("Becerra Panqueva")
+    facturation_formulary.find_element(by=By.NAME, value="billing_id_cliente").send_keys("1193476186")
+    facturation_formulary.find_element(by=By.NAME, value="billing_address_1").send_keys("Tv.49C#73Sur-12")
+    facturation_formulary.find_element(by=By.NAME, value="billing_address_2").send_keys("Sierra morena")
+    sleep(3)
+    driver_page.find_element(by=By.XPATH, value='//*[@id="billing_city_field"]/span/span[1]/span').click()
+    driver_page.find_element(by=By.XPATH, value='/html/body/span[2]/span/span[2]/ul/li[2]').click()
+    sleep(3)
+    facturation_formulary.find_element(by=By.NAME, value="billing_phone").send_keys("3016814746")
+    facturation_formulary.find_element(by=By.NAME, value="billing_email").send_keys("dbecerra655@gmail.com")
+    sleep(2)
+
+
+def accept_terms_and_buy(driver_page):
+    driver_page.find_element(by=By.XPATH, value='//*[@id="terms"]').click()
+    driver_page.find_element(by=By.XPATH, value='//*[@id="place_order"]').click()
+
+
 def main():
     # Pedimos al usuario que prducto va ha comprar
     url_product = create_url()
@@ -34,24 +75,18 @@ def main():
     theres_stock = review_stock(page_product)
 
     if theres_stock:
-        driver = webdriver.Firefox()
-        driver.get(url_product)
-        driver.find_element(by=By.NAME, value="add-to-cart").click()
+        # creamos el conductor
+        driver_page = created_driver_of_page(url_product)
         sleep(2)
-        final_buy = driver.find_element(by=By.CLASS_NAME, value="wc-proceed-to-checkout")
-        final_buy.find_element(by=By.TAG_NAME, value="a").click()
-        sleep(2)
-        facturation_and_send_formulary = driver.find_element(by=By.CLASS_NAME, value="woocommerce-billing-fields")
-        facturation_and_send_formulary.find_element(by=By.NAME, value="billing_first_name").send_keys("David")
-        facturation_and_send_formulary.find_element(by=By.NAME, value="billing_last_name").send_keys("Becerra Panqueva")
-        facturation_and_send_formulary.find_element(by=By.NAME, value="billing_id_cliente").send_keys("1193476186")
-        facturation_and_send_formulary.find_element(by=By.NAME, value="billing_address_1").send_keys("Tv.49C#73Sur-12")
-        facturation_and_send_formulary.find_element(by=By.NAME, value="billing_address_2").send_keys("Sierra morena")
-        facturation_and_send_formulary.find_element(by=By.NAME, value="billing_phone").send_keys("3016814746")
-        facturation_and_send_formulary.find_element(by=By.NAME, value="billing_email").send_keys("dbecerra655@gmail.com")
-        accept_order_and_terms = driver.find_element(by=By.ID, value="order_review")
-        accept_order_and_terms.find_element(by=By.ID, value="terms").click()
-        accept_order_and_terms.find_element(by=By.NAME, value="woocommerce_checkout_place_order")
+        # acceptamos las cookies
+        accept_cookies(driver_page)
+        # empezamos el proceso de compra
+        process_of_buy(driver_page)
+        # Llenamos el formulario de compra
+        complete_formulary(driver_page)
+        # Aceptamos la compra y realizamos la compra
+        accept_terms_and_buy(driver_page)
+
 
 
 if __name__ == '__main__':
